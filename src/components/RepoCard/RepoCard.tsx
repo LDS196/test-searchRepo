5b6d7d2698d513cb7685bada03fe305623d841e9
LDS196 <lihachevd87@gmail.com>
2023-05-26 18:50:49 +0400

    fix RepoCard.tsx

--- src/components/RepoCard/RepoCard.tsx
+++ src/components/RepoCard/RepoCard.tsx
@@ -10,26 +10,30 @@ export const RepoCard = () => {
     const repoItem = currentRepo?.node
 
     return (
-        <div className={s.main}>
-            <h3>About repository</h3>
-            <div className={s.intro}>
-                <img src={repoItem.owner.avatarUrl} alt="avatar" />
-                <span>Name:{repoItem.owner.name}</span>
-            </div>
-            <p>Name repository:{repoItem.name}</p>
-            <p>Number of stars:{repoItem.stargazers.totalCount}</p>
-            <p>Last commit:{repoItem.pushedAt}</p>
-            <div className={s.languages}>
-                <p>Languages</p>
-                {repoItem.languages.nodes.map((l) => (
-                    <span key={l.name}>{l.name}</span>
-                ))}
-            </div>
-            <p>Descriptions:{repoItem.description}</p>
+        <>
+            {repoItem && (
+                <div className={s.main}>
+                    <h3>About repository</h3>
+                    <div className={s.intro}>
+                        <img src={repoItem.owner.avatarUrl} alt="avatar" />
+                        <span>Name:{repoItem.owner.name}</span>
+                    </div>
+                    <p>Name repository:{repoItem.name}</p>
+                    <p>Number of stars:{repoItem.stargazers.totalCount}</p>
+                    <p>Last commit:{repoItem.pushedAt}</p>
+                    <div className={s.languages}>
+                        <p>Languages</p>
+                        {repoItem.languages.nodes.map((l) => (
+                            <span key={l.name}>{l.name}</span>
+                        ))}
+                    </div>
+                    <p>Descriptions:{repoItem.description}</p>
 
-            <Link to={"/"}>
-                <button>back to home</button>
-            </Link>
-        </div>
+                    <Link to={"/"}>
+                        <button>back to home</button>
+                    </Link>
+                </div>
+            )}
+        </>
     )
 }
